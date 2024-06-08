b56bad522d2d816b3c337d46c34ee05d93c3a1a6
aik2 <aik2mlj@gmail.com>
2024-06-08 12:22:36 +0800

    keymap & options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.scrolloff = 15
+vim.opt.relativenumber = false
+vim.opt.spell = false
+vim.opt.tabstop = 4
+vim.opt.guifont = "FiraCode Nerd Font:h11"
+
+vim.g.neovide_transparency = 0.9
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_theme = "auto"
