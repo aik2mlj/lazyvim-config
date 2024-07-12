5b25d8f03b8f03480c0bf821818edc2dde43dfe7
aik2 <aik2mlj@gmail.com>
2024-07-12 11:57:38 +0800

    neovide keys

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,11 @@ vim.opt.guifont = "FiraCode Nerd Font:h11"
 vim.g.neovide_transparency = 0.9
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_theme = "light"
+
+if vim.g.neovide then
+  vim.keymap.set("v", "<C-S-C>", '"+y') -- Copy
+  vim.keymap.set("n", "<C-S-V>", '"+p') -- Paste normal mode
+  vim.keymap.set("v", "<C-S-V>", '"+p') -- Paste visual mode
+  vim.keymap.set("c", "<C-S-V>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<C-S-V>", '<ESC>"+pi') -- Paste insert mode
+end
