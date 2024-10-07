edcb06c462dbee15c97ee9e61b7fcb9a55c2f1ff
aik2 <aik2mlj@gmail.com>
2024-10-07 13:40:17 -0700

    typst-preview.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -63,4 +63,12 @@ return {
       },
     },
   },
+  {
+    "chomosuke/typst-preview.nvim",
+    ft = "typst",
+    version = "0.3.*",
+    build = function()
+      require("typst-preview").update()
+    end,
+  },
 }
