84a39f7274a6b1a44b654c8d6ee974868df39864
aik2 <aik2mlj@gmail.com>
2024-11-10 16:03:26 -0800

    unset tabstop=4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@
 vim.opt.scrolloff = 15
 vim.opt.relativenumber = false
 vim.opt.spell = false
-vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+-- vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.guifont = "FiraCode Nerd Font:h11"
 
