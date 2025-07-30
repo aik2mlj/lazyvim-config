7371053d05c65ce8859c0bce9edc74553fd789ab
Lejun Min <aik2mlj@gmail.com>
2025-07-30 11:12:32 +0200

    claude-code.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -123,4 +123,18 @@ return {
       picker_integration = true,
     },
   },
+  {
+    "greggh/claude-code.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for git operations
+    },
+    config = function()
+      require("claude-code").setup({
+        window = {
+          split_ratio = 0.4,
+          position = "vertical",
+        },
+      })
+    end,
+  },
 }
