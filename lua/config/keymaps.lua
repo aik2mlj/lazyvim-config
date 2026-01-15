3160caeda1f00578b0b7b1e2874953f49f506ddc
Lejun Min <aik2mlj@gmail.com>
2026-01-14 21:21:15 -0800

    <leader><enter>: resume last search

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,6 +27,7 @@ vim.keymap.set("n", "U", "<c-r>")
 vim.keymap.set("n", "<leader>r", ":%s//g<Left><Left>", { desc = "Global Replace" })
 vim.keymap.set("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
 vim.keymap.set("n", "<leader>A", "<esc>ggVG", { desc = "Select All" })
+vim.keymap.set("n", "<leader><enter>", "<cmd>FzfLua resume<cr>", { desc = "Resume last search" })
 
 vim.keymap.set("x", "<M-J>", ":t'><CR>gv", { silent = true })
 vim.keymap.set("x", "<M-K>", ":t-1<CR>gv", { silent = true })
